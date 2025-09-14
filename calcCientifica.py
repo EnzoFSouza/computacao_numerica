892a6ba75b2921b5d3ed17b8ad4ae6fdd600b886
Enzo Souza <80353348+EnzoFSouza@users.noreply.github.com>
2025-09-14 17:30:54 -0300

    Adicionando mais funcoes

--- calcCientifica.py
+++ calcCientifica.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
+def f_abs(x):
+    return abs(x)
+
 def f_exp(x, N):
     e_aprox = 0
     for n in range(N):
@@ -30,6 +33,18 @@ def f_tg(x, N):
     tg_aprox = f_sen(x, N) / f_cos(x, N)
     return tg_aprox
 
+def f_sec(x, N):
+    sec_aprox = 1 / f_cos(x, N)
+    return sec_aprox
+
+def f_cossec(x, N):
+    cossec_aprox = 1 / f_sen(x, N)
+    return cossec_aprox
+
+def f_cotg(x, N):
+    cotg_aprox = 1 / f_tg(x, N)
+    return cotg_aprox
+
 def f_senh(x, N):
     senh_aprox = 0
     for n in range(N):
@@ -52,13 +67,17 @@ def f_tgh(x, N):
 
 def show_graph(resultado, nome, cor):
     plt.figure(figsize = (10, 6))
-    plt.plot(valores_continuos, resultado, label = nome, linewidth = 2, color = cor, linestyle = 'dashed')
+    plt.plot(valores_continuos, resultado, label = nome, linewidth = 2, color = cor)
     plt.legend(fontsize = 14)  
     plt.xlabel('Eixo x', fontsize = 16)
     plt.ylabel('Eixo y', fontsize = 16)
     plt.title(nome, fontsize = 20)
     plt.grid(True, linestyle = '-.', alpha = 0.7)
     plt.show()
+
+def show_abs():
+    resultado_abs = f_abs(valores_continuos)
+    show_graph(resultado_abs, "Função Módulo", "red")
 
 def show_exp():
     resultado_exp = f_exp(valores_continuos, N)
@@ -76,6 +95,18 @@ def show_tg():
     resultado_tg = f_tg(valores_continuos, N)
     show_graph(resultado_tg, "Função Tangente", "green")
 
+def show_sec():
+    resultado_sec = f_sec(valores_continuos, N)
+    show_graph(resultado_sec, "Função Secante", "blue")
+
+def show_cotg():
+    resultado_cotg = f_cotg(valores_continuos, N)
+    show_graph(resultado_cotg, "Função Cotangente", "blue")
+
+def show_cossec():
+    resultado_cossec = f_cossec(valores_continuos, N)
+    show_graph(resultado_cossec, "Função Cossecante", "blue")
+
 def show_senh():
     resultado_senh = f_senh(valores_continuos, N)
     show_graph(resultado_senh, "Função Seno Hiperbólico", "yellow")
@@ -88,18 +119,20 @@ def show_tgh():
     resultado_tgh = f_tgh(valores_continuos, N)
     show_graph(resultado_tgh, "Função Tangente Hiperbólica", "green")
 
-lista_funcoes = ["exp", "sen", "cos", "tg", "senh", "cosh", "tgh"]
+lista_opcoes = ["conta", "calc_func", "graficos", "sair"]
+lista_operacoes = ["+", "-", "*", "/", "%"]
+lista_funcoes = ["abs", "exp", "sen", "cos", "tg", "sec", "cossec", "cotg", "senh", "cosh", "tgh"]
 valores_continuos = np.linspace(-6, 6, 100)
 N = 15
 
 opcao = input("conta, calc_func, graficos, sair: ")
-while opcao != "conta" and opcao != "calc_func" and opcao != "graficos" and opcao != "sair":
+while opcao not in lista_opcoes:
     opcao = input("conta, calc_func, graficos, sair: ")
 
 while opcao != "sair":
     if opcao == "conta":
         operacao = input("Escolha uma das operacoes + - * / %: ")
-        while operacao != "+" and operacao != "-" and operacao != "*" and operacao != "/" and operacao != "%":
+        while operacao not in lista_operacoes:
             operacao = input("Escolha uma das operacoes + - * / %: ")
 
         try:
@@ -133,13 +166,17 @@ while opcao != "sair":
             print("Nao existe divisao por zero")
 
     elif opcao == "calc_func":
-        func = input("exp, sen, cos, tg, senh, cosh, tgh: ")
+        func = input("abs, exp, sen, cos, tg, sec, cossec, cotg, senh, cosh, tgh: ")
         while func not in lista_funcoes:
-            func = input("exp, sen, cos, tg, senh, cosh, tgh: ")
+            func = input("abs, exp, sen, cos, tg, sec, cossec, cotg, senh, cosh, tgh: ")
 
         valor = float(input("digite o valor: "))
         
-        if func == "exp":
+        if func == "abs":
+            resultado_abs = f_abs(valor)
+            print(resultado_abs)
+
+        elif func == "exp":
             resultado_exp = f_exp(valor, N)
             print(resultado_exp)
 
@@ -151,6 +188,18 @@ while opcao != "sair":
             resultado_cos = f_cos(valor, N)
             print(resultado_cos)
         
+        elif func == "sec":
+            resultado_sec = f_sec(valor, N)
+            print(resultado_sec)
+
+        elif func == "cossec":
+            resultado_cossec = f_cossec(valor, N)
+            print(resultado_cossec)
+        
+        elif func == "cotg":
+            resultado_cotg = f_cotg(valor, N)
+            print(resultado_cotg)
+
         elif func == "tg":
             resultado_tg = f_tg(valor, N)
             print(resultado_tg)
@@ -168,11 +217,14 @@ while opcao != "sair":
             print(resultado_tgh)
 
     elif opcao == "graficos":
-        graficos = input("exp, sen, cos, tg, senh, cosh, tgh: ")
+        graficos = input("abs, exp, sen, cos, tg, sec, cossec, cotg, senh, cosh, tgh: ")
         while graficos not in lista_funcoes:
-            graficos = input("exp, sen, cos, tg, senh, cosh, tgh: ")
+            graficos = input("abs, exp, sen, cos, tg, sec, cossec, cotg, senh, cosh, tgh: ")
 
-        if graficos == "exp":
+        if graficos == "abs":
+            show_abs()
+
+        elif graficos == "exp":
             show_exp()
 
         elif graficos == "sen":
@@ -184,6 +236,15 @@ while opcao != "sair":
         elif graficos == "tg":
             show_tg()
 
+        elif graficos == "sec":
+            show_sec()
+        
+        elif graficos == "cossec":
+            show_cossec()
+        
+        elif graficos == "cotg":
+            show_cotg()
+
         elif graficos == "senh":
             show_senh()
 
@@ -192,7 +253,7 @@ while opcao != "sair":
 
         elif graficos == "tgh":
             show_tgh()
-            
+
     opcao = input("conta, calc_func, graficos, sair: ")
-    while(opcao != "conta" and opcao != "calc_func" and opcao != "graficos" and opcao != "sair"):
+    while opcao not in lista_opcoes:
         opcao = input("conta, calc_func, graficos, sair: ")
